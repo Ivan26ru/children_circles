8384d2391c3f3591dbb707424a22389bb0e80bdb
i.pisarenko <i.pisarenko@crmguru.ru>
2022-02-10 11:09:32 +0300

    done block_trial_lesson

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
 
 	<link rel="stylesheet" href="assets/libs/animate/animate.css">
 
-<!--	slick-->
+	<!--	slick-->
 	<link rel="stylesheet" type="text/css" href="assets/libs/slick/slick.css">
 	<link rel="stylesheet" type="text/css" href="assets/libs/slick/slick-theme.css">
 
@@ -135,6 +135,34 @@
 	</div>
 </div>
 
+<div class="block_trial_lesson">
+	<div class="wrap">
+		<div class="btl_container">
+			<div class="btl_item">
+				<p class="btl_item_title">Пробное занятие бесплатно</p>
+				<p class="btl_item_text">Акция действует до 03.02.2022</p>
+				<p class="btl_item_text">Времени осталось: <span>2 дн. 16 час. 34 мин. 20 сек</span></p>
+				<p class="btl_item_total"><span class="btl_item_total_old">1000</span> = 0</p>
+			</div>
+			<div class="btl_item">
+				<div class="btl_form">
+					<div class="wrap_fire">
+						<img src="assets/img/fire.png" alt="">
+					</div>
+					<form action="">
+						<p class="btl_form_title">Текущая акция</p>
+						<div class="btl_form_wrap_input">
+							<input type="text" placeholder="Ваше имя">
+							<input type="text" placeholder="Контактный номер">
+							<input type="submit" value="Записаться">
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
 
 <!--[if lt IE 9]>
 <script src="assets/libs/html5shiv/es5-shim.min.js"></script>
@@ -150,7 +178,6 @@
 <script src="assets/libs/slick/slick.js" type="text/javascript" charset="utf-8"></script>
 
 <script src="assets/js/main.js"></script>
-
 
 
 </body>
