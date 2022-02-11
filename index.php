f0ed6d247f5aff9715fe3075ce500bd44267c77e
i.pisarenko <i.pisarenko@crmguru.ru>
2022-02-11 12:06:19 +0300

    done block_our_mugs

--- index.php
+++ index.php
@@ -297,6 +297,22 @@
 	</div>
 </div>
 
+<div class="block_our_mugs">
+	<div class="wrap">
+		<p class="title_block">Наши кружки</p>
+		<div class="bom_container">
+			<?php for ($i = 0; $i < 4; $i++) { ?>
+				<div class="bom_item">
+					<div class="bom_item_wrap_img">
+						<img src="assets/img/bom_img.png" alt="">
+					</div>
+					<a href="#" class="bom_item_title">Работы в ИЗО студии</a>
+				</div>
+			<? }; ?>
+		</div>
+	</div>
+</div>
+
 <!--[if lt IE 9]>
 <script src="assets/libs/html5shiv/es5-shim.min.js"></script>
 <script src="assets/libs/html5shiv/html5shiv.min.js"></script>
