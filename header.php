48142e52d0ede2409040429d3ee60ede808d6288
i.pisarenko <i.pisarenko@crmguru.ru>
2022-02-14 18:40:14 +0300

    add include header, footer

---
+++ header.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html><!--[if lt IE 7 ]>
+<html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]>
+<html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]>
+<html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!-->
+<html lang="ru"> <!--<![endif]-->
+
+<head>
+
+	<meta charset="utf-8">
+
+	<title>Главная</title>
+	<meta name="description" content="">
+
+	<link rel="shortcut icon" href="assets/img/favicon/favicon.ico" type="image/x-icon">
+	<link rel="apple-touch-icon" href="assets/img/favicon/apple-touch-icon.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="assets/img/favicon/apple-touch-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="assets/img/favicon/apple-touch-icon-114x114.png">
+
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+	<link rel="stylesheet" href="assets/css/reset.css">
+
+	<link rel="stylesheet" href="assets/libs/animate/animate.css">
+
+	<!--	slick-->
+	<link rel="stylesheet" type="text/css" href="assets/libs/slick/slick.css">
+	<link rel="stylesheet" type="text/css" href="assets/libs/slick/slick-theme.css">
+
+	<link rel="stylesheet" href="assets/fonts/fonts.css">
+	<link rel="stylesheet" href="assets/css/main.css">
+	<link rel="stylesheet" href="assets/css/media.css">
+
+	<script src="assets/libs/modernizr/modernizr.js"></script>
+
+
+</head>
+
+<body>
+
+<header>
+	<div class="wrap">
+		<a href="#" class="block_logo">
+			<div class="wrap_logo">
+				<img src="assets/img/circle.png" alt="">
+			</div>
+			<div class="block_logo_text">
+				<p class="blt_title">Детские кружки</p>
+				<div class="blt_line"></div>
+				<p class="blt_description">Детские кружки в Москве</p>
+			</div>
+		</a>
+		<div class="header_center">
+			<div class="hc_line1">
+				<a href="#">
+					<div class="hc_line1_wrap_img"><img src="assets/img/lic.png" alt=""></div>
+					Лицензия 77Л01 №0010539</a>
+			</div>
+			<div class="hc_line2">
+				<div class="menu_header">
+					<ul class="menu_header_ul">
+						<li><a href="#">Цены</a></li>
+						<li><a href="#">Обучение</a></li>
+						<li><a href="#">Секции</a></li>
+						<li><a href="#">Контакты</a></li>
+						<li><a href="#">Акции</a></li>
+						<li><a href="#">О нас</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<div class="header_info">
+			<p class="header_soc">
+				<a href="#" class="header_logo_soc"><img src="assets/img/tg.png" alt=""></a>
+				<a href="#" class="header_logo_soc"><img src="assets/img/ws.png" alt=""></a>
+				<a href="#" class="header_tell">8 (800) 758-770-02</a>
+			</p>
+			<p class="header_contact">с 8:00 до 22:00</p>
+			<a href="#" class="header_contact">info@dadastiopa.ru</a>
+		</div>
+	</div>
+</header>
