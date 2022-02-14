05c167e1bccb148fcdc3fa001f30daf64c8a0219
i.pisarenko <i.pisarenko@crmguru.ru>
2022-02-14 18:28:03 +0300

    done footer

--- index.php
+++ index.php
@@ -313,6 +313,72 @@
 	</div>
 </div>
 
+<div class="block_map">
+	<div class="wrap">
+		<div>Тут будет карта</div>
+		<div class="form">тут форма из cf7</div>
+	</div>
+</div>
+
+<footer>
+	<div class="wrap">
+		<div class="footer_container">
+			<div class="f_col f_col1">
+				<p class="footer_title">СТУДИЯ ДОШКОЛЬНОГО ОБРАЗОВАНИЯ</p>
+				<div class="footer_wrap_menu_1">
+					<ul class="fwm1_ul">
+						<li><a href="#">Цены</a></li>
+						<li><a href="#">Скидки</a></li>
+						<li><a href="#">Порядок обучения</a></li>
+						<li><a href="#">Контакты</a></li>
+						<li><a href="#">Акции</a></li>
+						<li><a href="#">О нас</a></li>
+					</ul>
+					<p class="f_best_offer">Лучшее предложение в Москве</p>
+				</div>
+			</div>
+			<div class="f_line"></div>
+			<div class="f_col f_col2">
+				<p class="footer_title">Кружки</p>
+				<div class="footer_wrap_menu_2">
+					<ul class="fwm2_ul">
+						<li>
+							<a href="#">ИЗО</a>
+							<a href="#">Раннее развитие</a>
+							<a href="#">Английский язык</a>
+							<a href="#">Дошкольная подготовка</a>
+							<a href="#">Театральная студия</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+			<div class="f_line"></div>
+			<div class="f_col f_col3">
+				<a href="#" class="block_logo">
+					<div class="wrap_logo">
+						<img src="assets/img/circle.png" alt="">
+					</div>
+					<div class="block_logo_text">
+						<p class="blt_title">Детские кружки</p>
+						<div class="blt_line"></div>
+						<p class="blt_description">Детские кружки в Москве</p>
+					</div>
+				</a>
+				<p class="f_adress">г.Москва, ул.Степана Супруна, д.4</p>
+				<div class="f_soc">
+					<p class="header_soc">
+						<a href="#" class="header_logo_soc"><img src="assets/img/tg.png" alt=""></a>
+						<a href="#" class="header_logo_soc"><img src="assets/img/ws.png" alt=""></a>
+						<a href="#" class="header_tell">8 (800) 758-770-02</a>
+					</p>
+				</div>
+				<p class="header_contact">с 8:00 до 22:00</p>
+				<a href="#" class="f_sitemap">Карта сайта</a>
+			</div>
+		</div>
+	</div>
+</footer>
+
 <!--[if lt IE 9]>
 <script src="assets/libs/html5shiv/es5-shim.min.js"></script>
 <script src="assets/libs/html5shiv/html5shiv.min.js"></script>
