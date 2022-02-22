08763d6d8020ff55555a2ce028e8cc031aca22b8
i.pisarenko <i.pisarenko@crmguru.ru>
2022-02-22 06:03:57 +0300

    done map

--- wp-content/themes/children-circles/include/block_map.php
+++ wp-content/themes/children-circles/include/block_map.php
@@ -1,9 +1,8 @@
 <div class="block_map">
-			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d39182.702762934205!2d37.58990117465706!3d55.74688767002491!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46b54afc73d4b0c9%3A0x3d44d6cc5757cf4c!2z0JzQvtGB0LrQstCw!5e0!3m2!1sru!2sru!4v1645203406917!5m2!1sru!2sru"
-					width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
-<!--	<div class="wrap">-->
-<!--		<div class="bm_container">-->
-<!--		</div>-->
-<!--		<div class="form">тут форма из cf7</div>-->
-<!--	</div>-->
+	<div class="bm_wrap_map">
+		<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d39182.702762934205!2d37.58990117465706!3d55.74688767002491!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46b54afc73d4b0c9%3A0x3d44d6cc5757cf4c!2z0JzQvtGB0LrQstCw!5e0!3m2!1sru!2sru!4v1645203406917!5m2!1sru!2sru" width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+	</div>
+	<div class="wrap">
+		<div class="form_map"><?php echo do_shortcode(get_field('cf7_map', 'option')); ?></div>
+	</div>
 </div>
