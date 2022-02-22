90ae09eb08d32628b4c29225f51cbac26f3faa9a
i.pisarenko <i.pisarenko@crmguru.ru>
2022-02-22 10:22:11 +0300

    done form style my_cf7_magenta my_cf7_silver

--- wp-content/themes/children-circles/include/block_training_with_us.php
+++ wp-content/themes/children-circles/include/block_training_with_us.php
@@ -7,14 +7,28 @@
 				<img class="bltwu_btn_youtube" src="<?php echo get_template_directory_uri(); ?>/assets/img/btn_youtube.png" alt="">
 			</a>
 			<a href="#" class="bltwu_block_1_item">
-			<img src="<?php echo get_template_directory_uri(); ?>/assets/img/2.png" alt="">
-			<img class="bltwu_btn_youtube" src="<?php echo get_template_directory_uri(); ?>/assets/img/btn_youtube.png" alt="">
+				<img src="<?php echo get_template_directory_uri(); ?>/assets/img/2.png" alt="">
+				<img class="bltwu_btn_youtube" src="<?php echo get_template_directory_uri(); ?>/assets/img/btn_youtube.png" alt="">
 			</a>
 		</div>
 
 		<div class="bltwu_block_2">
-			<div class="bltwu_block_2_table"><?php echo do_shortcode(get_field('training_with_us_table', 'option')); ?></div>
-			<div class="bltwu_block_2_cf7"><?php echo do_shortcode(get_field('training_with_us_cf7', 'option')); ?></div>
+			<div class="bltwu_block_2_table">
+				<div class="bltwu_block_2_table_wrap">
+					<div class="wrap_table_title">
+						<p class="table_title">Цены на кружки</p>
+						<p class="table_description">Лучшее предложение<br>в Москве</p>
+					</div>
+					<?php echo do_shortcode(get_field('training_with_us_table', 'option')); ?>
+				</div>
+			</div>
+			<div class="bltwu_block_2_cf7">
+				<div class="bltwu_form_wrap">
+					<div class="my_cf7_form my_cf7_magenta">
+						<?php echo do_shortcode(get_field('training_with_us_cf7', 'option')); ?>
+					</div>
+				</div>
+			</div>
 		</div>
 	</div>
 </div>
--- wp-content/themes/children-circles/include/block_trial_lesson.php
+++ wp-content/themes/children-circles/include/block_trial_lesson.php
@@ -8,19 +8,14 @@
 				<p class="btl_item_total"><span class="btl_item_total_old">1000</span> = 0</p>
 			</div>
 			<div class="btl_item">
+
 				<div class="btl_form">
 					<div class="icon_fire">
 						<img src="<?php echo get_template_directory_uri(); ?>/assets/img/fire.png" alt="">
 					</div>
-					<?php echo do_shortcode('[contact-form-7 id="72" title="Текущая акция"]'); ?>
-					<!--						<form action="">-->
-					<!--							<p class="btl_form_title">Текущая акция</p>-->
-					<!--							<div class="btl_form_wrap_input">-->
-					<!--								<input type="text" placeholder="Ваше имя">-->
-					<!--								<input type="text" placeholder="Контактный номер">-->
-					<!--								<input type="submit" value="Записаться">-->
-					<!--							</div>-->
-					<!--						</form>-->
+					<div class="my_cf7_form my_cf7_silver">
+						<?php echo do_shortcode('[contact-form-7 id="72" title="Текущая акция"]'); ?>
+					</div>
 				</div>
 			</div>
 		</div>
