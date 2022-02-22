3aef0a97e7941c5ea9c5dda8a4b509819097e520
i.pisarenko <i.pisarenko@crmguru.ru>
2022-02-22 06:04:11 +0300

    done block_training_with_us

--- wp-content/themes/children-circles/include/block_training_with_us.php
+++ wp-content/themes/children-circles/include/block_training_with_us.php
@@ -1,7 +1,20 @@
 <div class="block_training_with_us">
 	<div class="wrap">
 		<p class="title_block">Обучение у нас</p>
+		<div class="bltwu_block_1">
+			<a href="#" class="bltwu_block_1_item">
+				<img src="<?php echo get_template_directory_uri(); ?>/assets/img/1.png" alt="">
+				<img class="bltwu_btn_youtube" src="<?php echo get_template_directory_uri(); ?>/assets/img/btn_youtube.png" alt="">
+			</a>
+			<a href="#" class="bltwu_block_1_item">
+			<img src="<?php echo get_template_directory_uri(); ?>/assets/img/2.png" alt="">
+			<img class="bltwu_btn_youtube" src="<?php echo get_template_directory_uri(); ?>/assets/img/btn_youtube.png" alt="">
+			</a>
+		</div>
 
-
+		<div class="bltwu_block_2">
+			<div class="bltwu_block_2_table"><?php echo do_shortcode(get_field('training_with_us_table', 'option')); ?></div>
+			<div class="bltwu_block_2_cf7"><?php echo do_shortcode(get_field('training_with_us_cf7', 'option')); ?></div>
+		</div>
 	</div>
 </div>
