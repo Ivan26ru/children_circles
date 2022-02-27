61265b817c3d28ce752500278772cba55a5149d2
i.pisarenko <i.pisarenko@crmguru.ru>
2022-02-27 20:54:20 +0300

    done front map

--- wp-content/themes/children-circles/include/block_map.php
+++ wp-content/themes/children-circles/include/block_map.php
@@ -1,11 +1,141 @@
 <div class="block_map">
 	<div class="bm_wrap_map">
-		<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d39182.702762934205!2d37.58990117465706!3d55.74688767002491!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46b54afc73d4b0c9%3A0x3d44d6cc5757cf4c!2z0JzQvtGB0LrQstCw!5e0!3m2!1sru!2sru!4v1645203406917!5m2!1sru!2sru" width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+
+		<?php
+		if (!isset($yandexMapNumber)) {//условие для вывода одинаковых карт на одной странице
+			$yandexMapNumber = 1; ?>
+			<script src="https://api-maps.yandex.ru/2.1/?lang=ru_RU" type="text/javascript"></script>
+			<?php
+		} else {
+			$yandexMapNumber++;
+		}
+		$yandexMapAttrId = "yandexmap" . $yandexMapNumber;
+		$yandexMapInit = "init" . $yandexMapAttrId;
+		//		var_dump($yandexMapNumber);
+		//		var_dump($yandexMapAttrId);
+		//		var_dump($yandexMapInit);
+		?>
+
+
+		<div id="<?php echo $yandexMapAttrId; ?>" style="width: 100%; height: 500px"></div>
+		<script>
+			function <?php echo $yandexMapInit ?>() {
+				var myMap = new ymaps.Map(<?php echo $yandexMapAttrId ?>, {
+							center: [55.76, 37.64],
+							zoom: 10
+						}, {
+							searchControlProvider: 'yandex#search'
+						}),
+
+						// Создаем геообъект с типом геометрии "Точка".
+						myGeoObject = new ymaps.GeoObject({
+							// Описание геометрии.
+							geometry: {
+								type: "Point",
+								coordinates: [55.8, 37.8]
+							},
+							// Свойства.
+							properties: {
+								// Контент метки.
+								iconContent: 'Ссылка внутри',
+								hintContent: 'Ну давай уже тащи',
+								balloonContent: '<a href="#test">Тест ссылки</a>'
+							}
+						}, {
+							// Опции.
+							// Иконка метки будет растягиваться под размер ее содержимого.
+							preset: 'islands#blackStretchyIcon',
+							// Метку можно перемещать.
+							draggable: true
+						}),
+						myPieChart = new ymaps.Placemark([
+							55.847, 37.6
+						], {
+							// Данные для построения диаграммы.
+							data: [
+								{weight: 8, color: '#0E4779'},
+								{weight: 6, color: '#1E98FF'},
+								{weight: 4, color: '#82CDFF'}
+							],
+							iconCaption: "Диаграмма"
+						}, {
+							// Зададим произвольный макет метки.
+							iconLayout: 'default#pieChart',
+							// Радиус диаграммы в пикселях.
+							iconPieChartRadius: 30,
+							// Радиус центральной части макета.
+							iconPieChartCoreRadius: 10,
+							// Стиль заливки центральной части.
+							iconPieChartCoreFillStyle: '#ffffff',
+							// Cтиль линий-разделителей секторов и внешней обводки диаграммы.
+							iconPieChartStrokeStyle: '#ffffff',
+							// Ширина линий-разделителей секторов и внешней обводки диаграммы.
+							iconPieChartStrokeWidth: 3,
+							// Максимальная ширина подписи метки.
+							iconPieChartCaptionMaxWidth: 200
+						});
+
+				myMap.geoObjects
+						.add(myGeoObject)
+						.add(myPieChart)
+						.add(new ymaps.Placemark([55.684758, 37.738521], {
+							balloonContent: 'цвет <strong>воды пляжа бонди</strong>'
+						}, {
+							preset: 'islands#icon',
+							iconColor: '#0095b6'
+						}))
+						.add(new ymaps.Placemark([55.833436, 37.715175], {
+							balloonContent: '<strong>серобуромалиновый</strong> цвет'
+						}, {
+							preset: 'islands#dotIcon',
+							iconColor: '#735184'
+						}))
+						.add(new ymaps.Placemark([55.687086, 37.529789], {
+							balloonContent: 'цвет <strong>влюбленной жабы</strong>'
+						}, {
+							preset: 'islands#circleIcon',
+							iconColor: '#3caa3c'
+						}))
+						.add(new ymaps.Placemark([55.782392, 37.614924], {
+							balloonContent: 'цвет <strong>детской неожиданности</strong>'
+						}, {
+							preset: 'islands#circleDotIcon',
+							iconColor: 'yellow'
+						}))
+						.add(new ymaps.Placemark([55.642063, 37.656123], {
+							balloonContent: 'цвет <strong>красный</strong>'
+						}, {
+							preset: 'islands#redSportIcon'
+						}))
+						.add(new ymaps.Placemark([55.826479, 37.487208], {
+							balloonContent: 'цвет <strong>фэйсбука</strong>'
+						}, {
+							preset: 'islands#governmentCircleIcon',
+							iconColor: '#3b5998'
+						}))
+						.add(new ymaps.Placemark([55.694843, 37.435023], {
+							balloonContent: 'цвет <strong>носика Гены</strong>',
+							iconCaption: 'Очень длиннный, но невероятно интересный текст'
+						}, {
+							preset: 'islands#greenDotIconWithCaption'
+						}))
+						.add(new ymaps.Placemark([55.790139, 37.814052], {
+							balloonContent: 'цвет <strong>голубой</strong>',
+							iconCaption: 'Очень длиннный, но невероятно интересный текст'
+						}, {
+							preset: 'islands#blueCircleDotIconWithCaption',
+							iconCaptionMaxWidth: '50'
+						}));
+			}
+
+			ymaps.ready(<?php echo $yandexMapInit ?>);
+		</script>
+
 	</div>
 	<div class="wrap">
 		<div class="form_map">
 			<div class="my_cf7_form my_cf7_magenta">
 				<?php echo do_shortcode(get_field('cf7_map', 'option')); ?></div>
-			</div>
+		</div>
 	</div>
 </div>
