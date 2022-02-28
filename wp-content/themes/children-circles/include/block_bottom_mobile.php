02eea95b6ecc5e2d5912c72d6f29c29d92cae0fc
i.pisarenko <i.pisarenko@crmguru.ru>
2022-02-28 22:29:20 +0300

    fix block_bottom_mobile

--- wp-content/themes/children-circles/include/block_bottom_mobile.php
+++ wp-content/themes/children-circles/include/block_bottom_mobile.php
@@ -1,9 +1,9 @@
 <div class="block_bottom_mobile">
 	<div class="wrap">
 		<div class="bbm_container">
-			<a href="#" class="bbm_btn bbm_green">Цены</a>
-			<a href="#" class="bbm_btn bbm_green">Кружки</a>
-			<a href="#" class="bbm_btn bbm_call">Позвонить</a>
+			<a href="#" class="bbm_btn bbm_green popmake-114">Цены</a>
+			<a href="#" class="bbm_btn bbm_green popmake-114">Кружки</a>
+			<a href="tel:880075877002" class="bbm_btn bbm_call">Позвонить</a>
 		</div>
 	</div>
 </div>
