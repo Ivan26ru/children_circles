fbfe02ff9c398fec81cf9dd63b36db6f80c6984a
i.pisarenko <i.pisarenko@crmguru.ru>
2022-02-28 05:50:55 +0300

    done map connect to WordPress

--- wp-content/themes/children-circles/include/block_map.php
+++ wp-content/themes/children-circles/include/block_map.php
@@ -11,9 +11,6 @@
 		}
 		$yandexMapAttrId = "yandexmap" . $yandexMapNumber;
 		$yandexMapInit = "init" . $yandexMapAttrId;
-		//		var_dump($yandexMapNumber);
-		//		var_dump($yandexMapAttrId);
-		//		var_dump($yandexMapInit);
 		?>
 
 
@@ -21,111 +18,42 @@
 		<script>
 			function <?php echo $yandexMapInit ?>() {
 				var myMap = new ymaps.Map(<?php echo $yandexMapAttrId ?>, {
-							center: [55.76, 37.64],
-							zoom: 10
-						}, {
-							searchControlProvider: 'yandex#search'
-						}),
+					center: [55.76, 37.64],
+					zoom: 10
+				}, {
+					searchControlProvider: 'yandex#search'
+				});
 
-						// Создаем геообъект с типом геометрии "Точка".
-						myGeoObject = new ymaps.GeoObject({
-							// Описание геометрии.
-							geometry: {
-								type: "Point",
-								coordinates: [55.8, 37.8]
-							},
-							// Свойства.
-							properties: {
-								// Контент метки.
-								iconContent: 'Ссылка внутри',
-								hintContent: 'Ну давай уже тащи',
-								balloonContent: '<a href="#test">Тест ссылки</a>'
-							}
-						}, {
-							// Опции.
-							// Иконка метки будет растягиваться под размер ее содержимого.
-							preset: 'islands#blackStretchyIcon',
-							// Метку можно перемещать.
-							draggable: true
-						}),
-						myPieChart = new ymaps.Placemark([
-							55.847, 37.6
-						], {
-							// Данные для построения диаграммы.
-							data: [
-								{weight: 8, color: '#0E4779'},
-								{weight: 6, color: '#1E98FF'},
-								{weight: 4, color: '#82CDFF'}
-							],
-							iconCaption: "Диаграмма"
-						}, {
-							// Зададим произвольный макет метки.
-							iconLayout: 'default#pieChart',
-							// Радиус диаграммы в пикселях.
-							iconPieChartRadius: 30,
-							// Радиус центральной части макета.
-							iconPieChartCoreRadius: 10,
-							// Стиль заливки центральной части.
-							iconPieChartCoreFillStyle: '#ffffff',
-							// Cтиль линий-разделителей секторов и внешней обводки диаграммы.
-							iconPieChartStrokeStyle: '#ffffff',
-							// Ширина линий-разделителей секторов и внешней обводки диаграммы.
-							iconPieChartStrokeWidth: 3,
-							// Максимальная ширина подписи метки.
-							iconPieChartCaptionMaxWidth: 200
-						});
+				// Создаем геообъект с типом геометрии "Точка".
 
 				myMap.geoObjects
-						.add(myGeoObject)
-						.add(myPieChart)
-						.add(new ymaps.Placemark([55.684758, 37.738521], {
-							balloonContent: 'цвет <strong>воды пляжа бонди</strong>'
-						}, {
-							preset: 'islands#icon',
-							iconColor: '#0095b6'
-						}))
-						.add(new ymaps.Placemark([55.833436, 37.715175], {
-							balloonContent: '<strong>серобуромалиновый</strong> цвет'
+
+				<?php $args = array(
+						'post_type' => 'section',
+				);
+				$query = new WP_Query($args);
+
+
+				// Цикл
+				if ($query->have_posts()) {
+				while ($query->have_posts()) {
+				$query->the_post();?>
+						.add(new ymaps.Placemark([<?php the_field('lat'); ?>, <?php the_field('lng'); ?>], {
+							// balloonContent: '',
+							iconCaption: '<?php the_title(); ?>',
 						}, {
 							preset: 'islands#dotIcon',
 							iconColor: '#735184'
 						}))
-						.add(new ymaps.Placemark([55.687086, 37.529789], {
-							balloonContent: 'цвет <strong>влюбленной жабы</strong>'
-						}, {
-							preset: 'islands#circleIcon',
-							iconColor: '#3caa3c'
-						}))
-						.add(new ymaps.Placemark([55.782392, 37.614924], {
-							balloonContent: 'цвет <strong>детской неожиданности</strong>'
-						}, {
-							preset: 'islands#circleDotIcon',
-							iconColor: 'yellow'
-						}))
-						.add(new ymaps.Placemark([55.642063, 37.656123], {
-							balloonContent: 'цвет <strong>красный</strong>'
-						}, {
-							preset: 'islands#redSportIcon'
-						}))
-						.add(new ymaps.Placemark([55.826479, 37.487208], {
-							balloonContent: 'цвет <strong>фэйсбука</strong>'
-						}, {
-							preset: 'islands#governmentCircleIcon',
-							iconColor: '#3b5998'
-						}))
-						.add(new ymaps.Placemark([55.694843, 37.435023], {
-							balloonContent: 'цвет <strong>носика Гены</strong>',
-							iconCaption: 'Очень длиннный, но невероятно интересный текст'
-						}, {
-							preset: 'islands#greenDotIconWithCaption'
-						}))
-						.add(new ymaps.Placemark([55.790139, 37.814052], {
-							balloonContent: 'цвет <strong>голубой</strong>',
-							iconCaption: 'Очень длиннный, но невероятно интересный текст'
-						}, {
-							preset: 'islands#blueCircleDotIconWithCaption',
-							iconCaptionMaxWidth: '50'
-						}));
+
+				<?php }
+				} else {
+					// Постов не найдено
+				}
+				// Возвращаем оригинальные данные поста. Сбрасываем $post.
+				wp_reset_postdata();
+				?>
+				;
 			}
 
 			ymaps.ready(<?php echo $yandexMapInit ?>);
