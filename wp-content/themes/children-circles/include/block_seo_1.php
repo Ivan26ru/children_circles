b257b5a43f3218ebe8aa214aaf771e0a1f00d9e0
i.pisarenko <i.pisarenko@crmguru.ru>
2022-03-02 12:41:30 +0300

    fix design

--- wp-content/themes/children-circles/include/block_seo_1.php
+++ wp-content/themes/children-circles/include/block_seo_1.php
@@ -1,4 +1,4 @@
-<div class="block_seo_1">
+<div class="block_seo block_seo_1">
 	<div class="wrap_small">
 		<p class="title_block"><?php echo get_field('block_seo_1_title', 'option') ?></p>
 		<div class="bs_content">
--- wp-content/themes/children-circles/include/block_seo_2.php
+++ wp-content/themes/children-circles/include/block_seo_2.php
@@ -1,4 +1,4 @@
-<div class="block_seo_2">
+<div class="block_seo block_seo_2">
 	<div class="wrap_small">
 		<p class="title_block"><?php echo get_field('block_seo_2_title', 'option') ?></p>
 		<div class="bs_content">
