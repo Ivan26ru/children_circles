49ba46a0b0baf6372348b9cf6dda2db2a9a9393d
i.pisarenko <i.pisarenko@crmguru.ru>
2022-04-07 14:12:29 +0300

    fix wrap size

--- wp-content/themes/children-circles/include/block_seo_1.php
+++ wp-content/themes/children-circles/include/block_seo_1.php
@@ -1,5 +1,5 @@
 <div class="block_seo block_seo_1">
-	<div class="wrap_small">
+	<div class="wrap">
 		<p class="title_block"><?php echo get_field('block_seo_1_title', 'option') ?></p>
 		<div class="bs_content">
 			<?php echo get_field('block_seo_1_text', 'option') ?>
