84f831d46d6eb3da10aef2403b812d6cd2977a1c
i.pisarenko <i.pisarenko@crmguru.ru>
2022-04-07 14:12:39 +0300

    add phone icon

--- wp-content/themes/children-circles/include/block_bottom_mobile.php
+++ wp-content/themes/children-circles/include/block_bottom_mobile.php
@@ -3,7 +3,7 @@
 		<div class="bbm_container">
 			<a href="#" class="bbm_btn bbm_green popmake-114">Цены</a>
 			<a href="#" class="bbm_btn bbm_green popmake-114">Кружки</a>
-			<a href="tel:880075877002" class="bbm_btn bbm_call">Позвонить</a>
+			<a href="tel:880075877002" class="bbm_btn bbm_call">Позвонить <i class="fa fa-phone" aria-hidden="true"></i></i></a>
 		</div>
 	</div>
 </div>
