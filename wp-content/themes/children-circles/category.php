407111bbac5719ca808bb971978a2f39a63b9b8e
i.pisarenko <i.pisarenko@crmguru.ru>
2022-04-07 14:13:03 +0300

    add block_other_category

--- wp-content/themes/children-circles/category.php
+++ wp-content/themes/children-circles/category.php
@@ -13,7 +13,7 @@ get_header(); // подключаем header.php ?>
 <?php include "include/block_gratitude.php"; ?>
 <?php include "include/ratings_and_reviews.php"; ?>
 <?php include "include/block_special_offers.php"; ?>
-<?php include "include/block_teachers.php"; ?>
+<?php include "include/block_other_category.php"; ?>
 <?php include "include/block_posts.php"; ?>
 <?php include "include/block_map.php"; ?>
 <?php get_footer(); // подключаем footer.php ?>
