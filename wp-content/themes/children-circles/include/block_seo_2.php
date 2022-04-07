68a480923968eeb8481799928fa04400121649f2
i.pisarenko <i.pisarenko@crmguru.ru>
2022-04-07 21:13:29 +0300

    fix width wrap

--- wp-content/themes/children-circles/include/block_seo_2.php
+++ wp-content/themes/children-circles/include/block_seo_2.php
@@ -1,5 +1,5 @@
 <div class="block_seo block_seo_2">
-	<div class="wrap_small">
+	<div class="wrap">
 		<p class="title_block"><?php echo get_field('block_seo_2_title', 'option') ?></p>
 		<div class="bs_content">
 			<?php echo get_field('block_seo_2_text', 'option') ?>
