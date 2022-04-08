0fac484e50a8e12fc8283a759fc14c4cafefedea
i.pisarenko <i.pisarenko@crmguru.ru>
2022-04-08 12:56:47 +0300

    Override img caption shortcode to fix 10px issue.

--- wp-content/themes/children-circles/functions.php
+++ wp-content/themes/children-circles/functions.php
@@ -192,4 +192,20 @@ function site_h1(){
 		echo "Страница не найдена";
 	};
 };
+
+// Override img caption shortcode to fix 10px issue.
+add_filter('img_caption_shortcode', 'fix_img_caption_shortcode', 10, 3);
+
+function fix_img_caption_shortcode($val, $attr, $content = null) {
+	extract(shortcode_atts(array(
+		'id'    => '',
+		'align' => '',
+		'width' => '',
+		'caption' => ''
+	), $attr));
+
+	if ( 1 > (int) $width || empty($caption) ) return $val;
+
+	return '<div id="' . $id . '" class="wp-caption ' . esc_attr($align) . '" style="width: ' . (0 + (int) $width) . 'px">' . do_shortcode( $content ) . '<p class="wp-caption-text">' . $caption . '</p></div>';
+}
 ?>
