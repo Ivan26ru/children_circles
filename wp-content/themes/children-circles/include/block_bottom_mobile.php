ba2de2f912eafe8bbba35538df084b35a0b25667
i.pisarenko <i.pisarenko@crmguru.ru>
2022-04-20 10:14:23 +0300

    add link

--- wp-content/themes/children-circles/include/block_bottom_mobile.php
+++ wp-content/themes/children-circles/include/block_bottom_mobile.php
@@ -1,8 +1,8 @@
 <div class="block_bottom_mobile">
 	<div class="wrap">
 		<div class="bbm_container">
-			<a href="#" class="bbm_btn bbm_green popmake-114">Цены</a>
-			<a href="#" class="bbm_btn bbm_green popmake-114">Кружки</a>
+			<a href="/tseny/" class="bbm_btn bbm_green popmake-114">Цены</a>
+			<a href="/sektsii/" class="bbm_btn bbm_green popmake-114">Кружки</a>
 			<a href="tel:880075877002" class="bbm_btn bbm_call">Позвонить <i class="fa fa-phone" aria-hidden="true"></i></i></a>
 		</div>
 	</div>
