202366bcd03544ae0346eb727413d00e856249db
i.pisarenko <i.pisarenko@crmguru.ru>
2022-04-21 06:56:55 +0300

    rename name link

--- wp-content/themes/children-circles/include/block_bottom_mobile.php
+++ wp-content/themes/children-circles/include/block_bottom_mobile.php
@@ -2,7 +2,7 @@
 	<div class="wrap">
 		<div class="bbm_container">
 			<a href="/tseny/" class="bbm_btn bbm_green popmake-114">Цены</a>
-			<a href="/sektsii/" class="bbm_btn bbm_green popmake-114">Кружки</a>
+			<a href="/sektsii/" class="bbm_btn bbm_green popmake-114">Категории</a>
 			<a href="tel:880075877002" class="bbm_btn bbm_call">Позвонить <i class="fa fa-phone" aria-hidden="true"></i></i></a>
 		</div>
 	</div>
