ed719e07bc64abbfc7e33a35cc17285941af57d7
i.pisarenko <i.pisarenko@crmguru.ru>
2022-04-28 13:01:46 +0300

    integration block 'block_our_mugs' for wp

--- wp-content/themes/children-circles/include/block_our_mugs.php
+++ wp-content/themes/children-circles/include/block_our_mugs.php
@@ -1,31 +1,30 @@
 <div class="block_our_mugs">
 	<div class="wrap">
-		<p class="title_block">НАШИ КРУЖКИ</p>
+		<p class="title_block"><?php echo get_field('block_our_mugs_title', 'option') ?></p>
 		<div class="bom_container">
-				<div class="bom_item">
-					<div class="bom_item_wrap_img">
-						<img src="<?php echo get_template_directory_uri(); ?>/assets/img/bom_img.png" alt="">
+			<?php
+			if (have_rows('block_our_mugs_item', 'option')):
+				// Loop through rows.
+				while (have_rows('block_our_mugs_item', 'option')) :
+					the_row();
+					$title = get_sub_field('title');
+					$link = get_sub_field('link');
+					$img = get_url_img_is_sub_field('img', 'bom_item_img', get_template_directory_uri() . '/assets/img/image400.png')
+					?>
+					<div class="bom_item">
+						<div class="bom_item_wrap_img">
+							<img src="<?php echo $img; ?>" alt="<?php echo $title; ?>" title="<?php echo $title; ?>">
+						</div>
+						<a href="<?php echo $link; ?>" class="bom_item_title"><?php echo $title; ?></a>
 					</div>
-					<a href="#" class="bom_item_title">Работы в ИЗО студии</a>
-				</div>
-				<div class="bom_item">
-					<div class="bom_item_wrap_img">
-						<img src="<?php echo get_template_directory_uri(); ?>/assets/img/image 398.png" alt="">
-					</div>
-					<a href="#" class="bom_item_title">Театральные постановки</a>
-				</div>
-				<div class="bom_item">
-					<div class="bom_item_wrap_img">
-						<img src="<?php echo get_template_directory_uri(); ?>/assets/img/image 399.png" alt="">
-					</div>
-					<a href="#" class="bom_item_title">Английский язык</a>
-				</div>
-				<div class="bom_item">
-					<div class="bom_item_wrap_img">
-						<img src="<?php echo get_template_directory_uri(); ?>/assets/img/image 400.png" alt="">
-					</div>
-					<a href="#" class="bom_item_title">Раннее развитие</a>
-				</div>
+				<?php
+					// End loop.
+				endwhile;
+
+			// No value.
+			else :
+				// Do something...
+			endif; ?>
 		</div>
 	</div>
 </div>
