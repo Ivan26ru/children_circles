1b46a4bc8c455f7275e263b13cfe760b6b93a200
i.pisarenko <i.pisarenko@crmguru.ru>
2022-04-28 11:07:06 +0300

    fix visible post

--- wp-content/themes/children-circles/include/block_other_single.php
+++ wp-content/themes/children-circles/include/block_other_single.php
@@ -8,29 +8,34 @@
 			if ($categories) {
 				$category_ids = array();
 				foreach ($categories as $individual_category) $category_ids[] = $individual_category->term_id;
+				var_dump($category_ids);
+			} else {
+				$category_ids = '';
+			}
 
-				$args = array(
-						'category__in'     => $category_ids,
-						'post__not_in'     => array($post->ID),
-						'showposts'        => 5,
-						'orderby'          => 'rand',
-						'caller_get_posts' => 1
-				);
-				$my_query = new wp_query($args);
-				if ($my_query->have_posts()) {
-					while ($my_query->have_posts()) {
-						$my_query->the_post();
-						$this_img = get_the_post_thumbnail($post->ID, 'thumbnail');
-						$this_title = get_the_title();
-						$this_permalink = get_the_permalink();
-						if (!$this_img) {
-							$this_img = '<img src="' . get_template_directory_uri() . '/assets/img/1.png" alt="' . $this_title . '" title="' . $this_title . '"/>';
-						}
-						include "loop_other.php";
+			$args = array(
+					'category__in'     => $category_ids,
+					'post__not_in'     => array($post->ID),
+					'showposts'        => 5,
+					'orderby'          => 'rand',
+					'caller_get_posts' => 1,
+					'post_type'        => 'post'
+			);
+			$my_query = new wp_query($args);
+			if ($my_query->have_posts()) {
+				while ($my_query->have_posts()) {
+					$my_query->the_post();
+					$this_img = get_the_post_thumbnail($post->ID, 'thumbnail');
+					$this_title = get_the_title();
+					$this_permalink = get_the_permalink();
+					if (!$this_img) {
+						$this_img = '<img src="' . get_template_directory_uri() . '/assets/img/1.png" alt="' . $this_title . '" title="' . $this_title . '"/>';
 					}
+					include "loop_other.php";
 				}
-				wp_reset_query();
-			} ?>
+			}
+			wp_reset_query();
+			?>
 
 		</div>
 	</div>
