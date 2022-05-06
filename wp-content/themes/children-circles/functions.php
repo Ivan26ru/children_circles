0c30bc22d1c9b23115e58c9faacd8bd4c0c252b3
i.pisarenko <i.pisarenko@crmguru.ru>
2022-05-06 15:50:52 +0300

    fix tablepress new filter

--- wp-content/themes/children-circles/functions.php
+++ wp-content/themes/children-circles/functions.php
@@ -130,6 +130,7 @@ if (!function_exists('add_styles')) { // если ф-я уже есть в до�
 		wp_enqueue_style('slick-theme', get_template_directory_uri() . '/assets/libs/slick/slick-theme.css');
 		wp_enqueue_style('main', get_template_directory_uri() . '/style.css?' . date('U'));
 		wp_enqueue_style('main_media', get_template_directory_uri() . '/assets/css/media.css');
+		wp_enqueue_style('tablepress_style', get_template_directory_uri() . '/assets/css/tablepress_style.css');
 	}
 }
 
@@ -209,5 +210,52 @@ function fix_img_caption_shortcode($val, $attr, $content = null) {
 	if ( 1 > (int) $width || empty($caption) ) return $val;
 
 	return '<div id="' . $id . '" class="wp-caption ' . esc_attr($align) . '" style="width: ' . (0 + (int) $width) . 'px">' . do_shortcode( $content ) . '<p class="wp-caption-text">' . $caption . '</p></div>';
+}
+
+
+// фильтр для заголовков таблицы
+add_filter('tablepress_print_name_html_tag', 'tablepress_print_name_html_tag_function', 10, 1);
+function tablepress_print_name_html_tag_function($str) {
+	$str = 'h3';
+	return $str;
+}
+
+// фильтр для описания таблицы
+add_filter('tablepress_print_description_html_tag', 'tablepress_print_description_html_tag_function', 10, 1);
+function tablepress_print_description_html_tag_function($str) {
+	$str = 'span';
+	return $str;
+}
+
+// добавление таблице обертки и контейнера для заголовков
+add_filter('tablepress_table_output', 'tablepress_table_output_function', 10, 1);
+function tablepress_table_output_function($table) {
+	$tablepress__header_tag = 'div';
+	$tablepress__header_class = 'tablepress__header';
+	$tablepress__header_start = '<' . $tablepress__header_tag . ' class="' . $tablepress__header_class . '">';
+	$tablepress__header_end = '</' . $tablepress__header_tag . '>';
+
+	$is_table_name = substr_count($table, '"tablepress-table-name');
+	$is_table_description = substr_count($table, '"tablepress-table-description');
+
+	$replacement = $tablepress__header_start . '${0}' . $tablepress__header_end;
+	$subject = $table;
+	if ($is_table_name && $is_table_description) {
+		$pattern = '/^[\W\w]*?<\/span>/i';
+		$return_table = preg_replace($pattern, $replacement, $subject);
+	} elseif ($is_table_name) {
+		$pattern = '/^[\W\w]*?<\/h3>/i';
+		$return_table = preg_replace($pattern, $replacement, $subject);
+	} elseif ($is_table_description) {
+		$pattern = '/^[\W\w]*?<\/span>/i';
+		$return_table = preg_replace($pattern, $replacement, $subject);
+	} else {
+		$return_table = $table;
+	}
+
+	$return_table = '<div class="tablepress__wrap">' . $return_table . '</div>';
+
+	$resoult = $return_table;
+	return $resoult;
 }
 ?>
--- wp-content/themes/children-circles/include/block_training_with_us.php
+++ wp-content/themes/children-circles/include/block_training_with_us.php
@@ -1,19 +1,13 @@
 <div class="block_training_with_us">
 	<div class="wrap">
-		<p class="title_block">Обучение у нас</p>		<br>
+		<p class="title_block">Обучение у нас</p>        <br>
 		<?php echo do_shortcode(get_field('training_with_us_video', 'option')); ?>
-<!--		<div class="bltwu_block_1">-->
-<!--		</div>-->
+		<!--		<div class="bltwu_block_1">-->
+		<!--		</div>-->
 
 		<div class="bltwu_block_2" id="bltwu_block_2">
 			<div class="bltwu_block_2_table">
-				<div class="bltwu_block_2_table_wrap">
-					<div class="wrap_table_title">
-						<p class="table_title">Цены на кружки</p>
-						<p class="table_description">Лучшее предложение<br>в Москве</p>
-					</div>
-					<?php echo do_shortcode(get_field('training_with_us_table', 'option')); ?>
-				</div>
+				<?php echo do_shortcode(get_field('training_with_us_table', 'option')); ?>
 			</div>
 			<div class="bltwu_block_2_cf7">
 				<div class="bltwu_form_wrap" id="bltwu_form_wrap">
