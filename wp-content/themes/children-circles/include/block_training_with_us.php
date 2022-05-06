7284ebd3fc5ecea757753c6c046d3d11a7c74fea
i.pisarenko <i.pisarenko@crmguru.ru>
2022-05-06 13:23:39 +0300

    add sticky block #bltwu_form_wrap

--- wp-content/themes/children-circles/include/block_training_with_us.php
+++ wp-content/themes/children-circles/include/block_training_with_us.php
@@ -5,7 +5,7 @@
 <!--		<div class="bltwu_block_1">-->
 <!--		</div>-->
 
-		<div class="bltwu_block_2">
+		<div class="bltwu_block_2" id="bltwu_block_2">
 			<div class="bltwu_block_2_table">
 				<div class="bltwu_block_2_table_wrap">
 					<div class="wrap_table_title">
@@ -16,7 +16,7 @@
 				</div>
 			</div>
 			<div class="bltwu_block_2_cf7">
-				<div class="bltwu_form_wrap">
+				<div class="bltwu_form_wrap" id="bltwu_form_wrap">
 					<div class="my_cf7_form my_cf7_magenta">
 						<?php echo do_shortcode(get_field('training_with_us_cf7', 'option')); ?>
 					</div>
