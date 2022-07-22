cbd56ea0e76629fc5d7724e0a83a7e62facba270
i.pisarenko <i.pisarenko@crmguru.ru>
2022-07-22 10:39:36 +0300

    пофиксил страницу оплаты

--- wp-content/themes/children-circles/woocommerce/checkout/payment.php
+++ wp-content/themes/children-circles/woocommerce/checkout/payment.php
@@ -23,19 +23,19 @@ if ( ! wp_doing_ajax() ) {
 ?>
 <div id="payment" class="woocommerce-checkout-payment ">
 
-<!--	--><?php //if ( WC()->cart->needs_payment() ) : ?>
-<!--		<ul class="wc_payment_methods payment_methods methods">-->
-<!--			--><?php
-//			if ( ! empty( $available_gateways ) ) {
-//				foreach ( $available_gateways as $gateway ) {
-//					wc_get_template( 'checkout/payment-method.php', array( 'gateway' => $gateway ) );
-//				}
-//			} else {
-//				echo '<li class="woocommerce-notice woocommerce-notice--info woocommerce-info">' . apply_filters( 'woocommerce_no_available_payment_methods_message', WC()->customer->get_billing_country() ? esc_html__( 'Sorry, it seems that there are no available payment methods for your state. Please contact us if you require assistance or wish to make alternate arrangements.', 'woocommerce' ) : esc_html__( 'Please fill in your details above to see available payment methods.', 'woocommerce' ) ) . '</li>'; // @codingStandardsIgnoreLine
-//			}
-//			?>
-<!--		</ul>-->
-<!--	--><?php //endif; ?>
+	<?php if ( WC()->cart->needs_payment() ) : ?>
+		<ul class="wc_payment_methods payment_methods methods">
+			<?php
+			if ( ! empty( $available_gateways ) ) {
+				foreach ( $available_gateways as $gateway ) {
+					wc_get_template( 'checkout/payment-method.php', array( 'gateway' => $gateway ) );
+				}
+			} else {
+				echo '<li class="woocommerce-notice woocommerce-notice--info woocommerce-info">' . apply_filters( 'woocommerce_no_available_payment_methods_message', WC()->customer->get_billing_country() ? esc_html__( 'Sorry, it seems that there are no available payment methods for your state. Please contact us if you require assistance or wish to make alternate arrangements.', 'woocommerce' ) : esc_html__( 'Please fill in your details above to see available payment methods.', 'woocommerce' ) ) . '</li>'; // @codingStandardsIgnoreLine
+			}
+			?>
+		</ul>
+	<?php endif; ?>
 
 	<div class="woo-block__wrap form-row place-order">
 		<noscript>
@@ -50,7 +50,7 @@ if ( ! wp_doing_ajax() ) {
 
 		<?php do_action( 'woocommerce_review_order_before_submit' ); ?>
 
-		<?php echo apply_filters( 'woocommerce_order_button_html', '<button type="submit" class="button alt woo-block__item" name="woocommerce_checkout_place_order" id="place_order" value="' . esc_attr( $order_button_text ) . '" data-value="' . esc_attr( $order_button_text ) . '">' . esc_html( $order_button_text ) . '</button>' ); // @codingStandardsIgnoreLine ?>
+		<?php echo apply_filters( 'woocommerce_order_button_html', '<div class="woo-block__item woo-block__item_submit"><button type="submit" class="button alt" name="woocommerce_checkout_place_order" id="place_order" value="' . esc_attr( $order_button_text ) . '" data-value="' . esc_attr( $order_button_text ) . '">' . esc_html( $order_button_text ) . '</button></div>' ); // @codingStandardsIgnoreLine ?>
 
 		<?php do_action( 'woocommerce_review_order_after_submit' ); ?>
 
