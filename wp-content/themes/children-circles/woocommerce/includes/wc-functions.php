38a4bc56a7d6c667fbabc9700546a82d4d837749
i.pisarenko <i.pisarenko@crmguru.ru>
2022-07-22 08:35:09 +0300

    доделал страницу товара, черновик страницы заказа

--- wp-content/themes/children-circles/woocommerce/includes/wc-functions.php
+++ wp-content/themes/children-circles/woocommerce/includes/wc-functions.php
@@ -12,3 +12,9 @@ if ( ! function_exists( 'woocommerce_single_variation' ) ) {
 		echo '<div class="woo-block__item"><div class="woocommerce-variation single_variation"></div><label>Стоимость занятия</label></div>';
 	}
 }
+
+add_filter( 'woocommerce_order_button_text', 'truemisha_order_button_text' );
+
+function truemisha_order_button_text( $button_text ) {
+	return 'ПЕРЕЙТИ К ОПЛАТЕ';
+}
